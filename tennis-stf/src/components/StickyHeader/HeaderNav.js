53f34fca16fd02727cbfd810e275e462be4b0bb9
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-11-28 19:55:40 +0100

    Bah, abandon
    I don't understand enough of bootstrap to get this working the way I want - do it with react instead but no bootstrap

--- tennis-stf/src/components/StickyHeader/HeaderNav.js
+++ tennis-stf/src/components/StickyHeader/HeaderNav.js
@@ -1,34 +1,42 @@
 import React from "react";
 import "./HeaderNav.css";
-import Stack from "react-bootstrap/Stack";
 import Nav from "react-bootstrap/Nav";
-
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 const HeaderNav = () => {
-
-    return (
-        <Nav id="nav-list" class="header-nav" defaultActiveKey="/index">
-            <Nav.Item class="hover-underline-animation">
-                <Nav.Link href="/index" id="nav-index"> Om STF</Nav.Link>
-            </Nav.Item>
-            <div className="vr" />
-            <Nav.Item class="hover-underline-animation">
-                <Nav.Link href="banor.html" id="nav-banor">Banor</Nav.Link>
-            </Nav.Item>
-            <div className="vr" />
-            <Nav.Item class="hover-underline-animation">
-                <Nav.Link href="omkl.html" id="nav-omkl">Omklädningsrum</Nav.Link>
-            </Nav.Item>
-            <div className="vr" />
-            <Nav.Item class="hover-underline-animation">
-                <Nav.Link href="bastu.html"  id="nav-bastu" >Bastu</Nav.Link>
-            </Nav.Item>
-            <div className="vr" />
-            <Nav.Item class="hover-underline-animation">
-               <i class="fa-solid fa-bars fa-lg hamburger"></i>
-            </Nav.Item>
-        </Nav>
-    )
+  return (
+    <Nav id="nav-list" class="header-nav" defaultActiveKey="/index">
+      <Nav.Item class="hover-underline-animation">
+        <Nav.Link href="/index" id="nav-index">
+          {" "}
+          Om STF
+        </Nav.Link>
+      </Nav.Item>
+      <div className="vr" />
+      <Nav.Item class="hover-underline-animation">
+        <Nav.Link href="banor.html" id="nav-banor">
+          Banor
+        </Nav.Link>
+      </Nav.Item>
+      <div className="vr" />
+      <Nav.Item class="hover-underline-animation">
+        <Nav.Link href="omkl.html" id="nav-omkl">
+          Omklädningsrum
+        </Nav.Link>
+      </Nav.Item>
+      <div className="vr" />
+      <Nav.Item class="hover-underline-animation">
+        <Nav.Link href="bastu.html" id="nav-bastu">
+          Bastu
+        </Nav.Link>
+      </Nav.Item>
+      <div className="vr" />
+      <Nav.Item class="hover-underline-animation">
+        <i class="fa-solid fa-bars fa-lg hamburger"></i>
+      </Nav.Item>
+    </Nav>
+  );
 };
 
 export default HeaderNav;
--- tennis-stf/src/components/StickyHeader/StickyHeader.js
+++ tennis-stf/src/components/StickyHeader/StickyHeader.js
@@ -2,21 +2,72 @@ import React from "react";
 import HeaderNav from "./HeaderNav";
 import Logo from "../Logo/Logo";
 import "./StickyHeader.css";
-import Stack from "react-bootstrap/Stack"
+import Stack from "react-bootstrap/Stack";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const StickyHeader = (props) => {
-
-
-    return(
-        <Stack as="header" direction="horizontal" className="header" >
-            <div >
-                <Logo text="Skänninge Tennisförening" wrap="wrap" />
-            </div>
-            <div className="ms-auto">
-                <HeaderNav  />
-            </div>
-        </Stack>      
-    )
+  return (
+    <Navbar className="header" sticky="top" expand="lg">
+      <Navbar.Brand className="logo" href="#home">
+        Skänninge Tennisförening
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="nav me-auto">
+          <Nav.Link
+            className="hover-underline-animation"
+            href="#home"
+            id="nav-index"
+          >
+            Om STF
+          </Nav.Link>
+          <Nav.Link
+            className="hover-underline-animation"
+            href="#banor"
+            id="nav-banor"
+          >
+            Banor
+          </Nav.Link>
+          <Nav.Link
+            className="hover-underline-animation"
+            href="#omkl"
+            id="nav-omkl"
+          >
+            Omklädningsrum
+          </Nav.Link>
+          <Nav.Link
+            className="hover-underline-animation"
+            href="#bastu"
+            id="nav-bastu"
+          >
+            Bastu
+          </Nav.Link>
+          <NavDropdown drop="start" title="Boka">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    // <Stack as="header" direction="horizontal" className="header">
+    //   <div>
+    //     <Logo text="Skänninge Tennisförening" wrap="wrap" />
+    //   </div>
+    //   <div className="ms-auto">
+    //     <HeaderNav />
+    //   </div>
+    // </Stack>
+  );
 };
 
 export default StickyHeader;
