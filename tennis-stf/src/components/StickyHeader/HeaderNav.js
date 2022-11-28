2e1e0b985ab818644adbb7a674e3c1ae932fe3e4
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-11-28 21:34:06 +0100

    Header re-worked

--- tennis-stf/src/components/StickyHeader/HeaderNav.js
+++ tennis-stf/src/components/StickyHeader/HeaderNav.js
@@ -1,29 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HeaderNav.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
+const Menu = (props) => {
+  let classes = "menu nav-link " + props.additionalClass;
+  console.log(classes);
+  return (
+    <div className={classes}>
+      <li className="hover-underline-animation">
+        <a href="index.html" id="nav-index">
+          {" "}
+          Om STF
+        </a>
+      </li>
+      <li className="hover-underline-animation">
+        <a href="banor.html" id="nav-banor">
+          Banor
+        </a>
+      </li>
+      <li className="hover-underline-animation">
+        <a href="omkl.html" id="nav-omkl">
+          Omklädningsrum
+        </a>
+      </li>
+      <li className="hover-underline-animation">
+        <a href="bastu.html" id="nav-bastu">
+          Bastu
+        </a>
+      </li>
+    </div>
+  );
+};
+
+const BookingMenu = () => {
+  return (
+    <>
+      <span className="nav-link">Boka Omklädningsrum</span>
+      <span className="nav-link">Boka Andra Rum</span>
+    </>
+  );
+};
 
 const HeaderNav = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-    return(
-        <nav>
-            <ul id="nav-list" class="header-nav">
-            <li class="hover-underline-animation">
-                <a href="index.html" id="nav-index"> Om STF</a>
-            </li>
-            <li class="hover-underline-animation">
-                <a href="banor.html" id="nav-banor">Banor</a>
-            </li>
-            <li class="hover-underline-animation">
-                <a href="omkl.html" id="nav-omkl">Omklädningsrum</a>
-            </li>
-            <li class="hover-underline-animation">
-                <a href="bastu.html" id="nav-bastu">Bastu</a>
-            </li>
-            <li class="hover-underline-animation">
-                <i class ="fa-solid fa-bars fa-lg hamburger"></i>
-            </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <ul id="nav-list" class="header-nav">
+        <Menu additionalClass="collapse" />
+        {toggleMenu ? (
+          <li>
+            <RiCloseLine
+              className="hamburger"
+              onClick={() => setToggleMenu(false)}
+            />
+          </li>
+        ) : (
+          <li>
+            <RiMenu3Line
+              className="hamburger"
+              onClick={() => setToggleMenu(true)}
+            />
+          </li>
+        )}
+        {toggleMenu && (
+          <div className="navbar_menu_container scale-up-tr">
+            <Menu additionalClass="small-only" />
+            <BookingMenu />
+          </div>
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default HeaderNav;
--- tennis-stf/src/components/StickyHeader/StickyHeader.js
+++ tennis-stf/src/components/StickyHeader/StickyHeader.js
@@ -4,14 +4,12 @@ import Logo from "../Logo/Logo";
 import "./StickyHeader.css";
 
 const StickyHeader = (props) => {
-
-    
-    return(
-        <header className="header flex-as-row" >
-            <Logo text="Skänninge Tennisförening" wrap="wrap" />
-            <HeaderNav />
-        </header>      
-    )
+  return (
+    <header className="header flex-as-row">
+      <Logo text="Skänninge Tennisförening" wrap="wrap" />
+      <HeaderNav />
+    </header>
+  );
 };
 
 export default StickyHeader;
