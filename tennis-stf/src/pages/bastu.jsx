ff882e5465b562beb97bb4a9bac98c61f3bbe3a0
Björn Rundquist <bjorn.rundqvist@gmail.com>
2022-12-01 17:29:54 +0100

    Solved sass structure
    While this will still import all the stylesheets to all of our pages, doing a sass .pageName { page css } and having the page be wrapped in a section with that name solves overlapping styles between pages

--- tennis-stf/src/pages/bastu.jsx
+++ tennis-stf/src/pages/bastu.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "./styles/bastu.css";
 
-const Bastu =  () => {
-    //Note to self- this doesn't work
-    const queryString = window.location.search;
-    console.log(queryString);
-    const urlParams = new URLSearchParams(queryString);
-    console.log(urlParams);
-    return (
-        <section className="page-container">
-            <p>This is the bastu page</p>
-            {/* Dagens bokningar före bokningsformuläret här också */}
-        </section>
-    )
-}
-
+const Bastu = () => {
+  //Note to self- this doesn't work
+  const queryString = window.location.search;
+  console.log(queryString);
+  const urlParams = new URLSearchParams(queryString);
+  console.log(urlParams);
+  return (
+    <section className="bastu">
+      <section className="page-container">
+        <p>This is the bastu page</p>
+        {/* Dagens bokningar före bokningsformuläret här också */}
+      </section>
+    </section>
+  );
+};
 
 export default Bastu;
