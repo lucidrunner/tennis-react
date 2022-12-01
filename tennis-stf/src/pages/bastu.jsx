6b02ed75acf19c6e82143c4725db2a8320dd74ed
Björn Rundquist <50362568+lucidrunner@users.noreply.github.com>
2022-12-01 16:29:24 +0100

    Push to main

--- tennis-stf/src/pages/bastu.jsx
+++ tennis-stf/src/pages/bastu.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./styles/bastu.css";
 
 const Bastu =  () => {
+    //Note to self- this doesn't work
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    console.log(urlParams);
     return (
         <section className="page-container">
             <p>This is the bastu page</p>
